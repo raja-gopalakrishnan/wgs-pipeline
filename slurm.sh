f66dffb6c8773ea93584d4d382de03809b30e4a9
Gopalakrishnan <rg161@compute-a-16-171.o2.rc.hms.harvard.edu>
2019-01-08 21:09:48 -0500

    Added slurm.sh and cluster.yaml files

---
+++ slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -p priority
+#SBATCH -t 12:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH -n 1
+#SBATCH -e superjob.err
+#SBATCH -o superjob.log
+#SBATCH -J ChIPSeq-snakemake
+
+#snakemake -n -R `snakemake --lc --li --lp` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
+
+snakemake -p --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
+
+#"sbatch -p short -n 4 -t 6:00:00 --mem-per-cpu=4G -e snakemake.err -o snakemake.log"
